465c10290c3ca69bfe1c3b245b42df0ebf1f4ea5
margarunka <margarunka26@gmail.com>
2021-11-01 14:00:41 +0200

    create elements for employees-list-item adding

--- src/components/emloyees-list/employees-list.js
+++ src/components/emloyees-list/employees-list.js
@@ -2,12 +2,18 @@ import EmployeesListItem from "../emloyees-list-item/employees-list-item";
 
 import './employees-list.css';
 
-const EmployeesList = () => {
+const EmployeesList = ({data}) => {
+
+    const elements = data.map(item => {
+        return (
+            // <EmployeesListItem name={item.name} salary={item.salary} /> два варіанти запису
+            <EmployeesListItem {...item}/> // spread operator
+        )
+    })
+
     return (
         <ul className="app-list list-group">
-            <EmployeesListItem name="Harry P." salary={800}/>
-            <EmployeesListItem name="Anabel S." salary={3000}/>
-            <EmployeesListItem name="George N." salary={550}/>
+            {elements}
         </ul>
     )
 }
